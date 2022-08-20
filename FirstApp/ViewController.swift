e3819bb504c154a2f7523ee2007a2870bf40a548
ankh04 <ankh04@icloud.com>
2022-08-20 12:12:24 +0800

    游戏逻辑完成 学习到了outlet变量，把UI组件与变量绑定

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -9,14 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
     var currentValue: Int = 1
+    var targetValue: Int = 0
+    var round: Int = 0
+    var score: Int = 0
+    @IBOutlet var roundLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var target: UILabel!
+    
+    func updateScore() {
+        score += 100 - abs(currentValue - targetValue)
+        round += 1
+        scoreLabel.text = String(score)
+        roundLabel.text = "\(round)"
+    }
+    
+    func reset() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        target.text = String(targetValue) // 或者是"\(targetValue)"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+        reset()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func showAlert() {
-        let message = "现在滑动块的值是：\(currentValue)"
+        let message = "现在滑动块的值是：\(currentValue)" + "\n目标值为：\(targetValue)"
         let alert = UIAlertController(
             title: "你好！",
             message: message,
@@ -30,6 +54,9 @@ class ViewController: UIViewController {
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        updateScore()
+        reset()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
