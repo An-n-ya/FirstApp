eeabe39d987e77050d48dc6540cb9c701bf4a97a
ankh04 <ankh04@icloud.com>
2022-08-25 13:29:32 +0800

    外观美化，增加游戏介绍页

---
+++ FirstApp/AboutViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AboutViewController.swift
+//  FirstApp
+//
+//  Created by 张环宇 on 2022/8/25.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -42,6 +42,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         startNewGame()
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(
+            top: 0, left: 14, bottom: 0, right: 14
+        )
+        
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")
+        let trackLeftResizable = trackLeftImage?.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = UIImage(named: "SliderTrackRight")
+        let trackRightResizable = trackRightImage?.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
     }
     
     @IBAction func showAlert() {
@@ -62,7 +80,7 @@ class ViewController: UIViewController {
         } else {
             title = "差太远了！"
         }
-        let message = "你的分数为：\(100 - difference)"
+        let message = "你的分数为：\(points)"
         
         let alert = UIAlertController(
             title: title,
